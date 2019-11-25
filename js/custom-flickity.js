6be90a534e9088f4dedbe24b6e6de55c13dfaac1
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-25 15:26:27 -0800

    fix css size for tablet

--- js/custom-flickity.js
+++ js/custom-flickity.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     const elem = document.querySelector('.main-carousel');
     const flkty = new Flickity( elem, {
     cellAlign: 'left',
     wrapAround: true,
     freeScroll: true, 
-    prevNextButtons: false 
+    prevNextButtons: false
     });
+
 });
