fb8de559bbd9fa5ebfd33cccb34659dffb5e2d72
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-25 16:47:37 -0800

    fix fixed nav bar

--- js/custom-subBtn.js
+++ js/custom-subBtn.js
@@ -1,11 +1,10 @@
-function subscribe() {
-    const email = document.getElementById("email");
-    if (!email.checkValidity()) {
-        alert("Please enter a valid e-mail");
-    } else {
-        alert("Thanks for subscribing!");
-        event.preventDefault();
-        email.value=null; 
-    };
-};
+document.addEventListener("DOMContentLoaded", function () {
 
+    const email=document.getElementById("email");
+        if (!email.checkValidity()) {
+            alert("Please enter a valid e-mail");
+        } else {
+            alert("Thanks for subscribing!");
+            email.value = null;
+        }
+});
