89aa4fbcdb092d04eea4967dc604e7dd6d9fc9ac
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-25 18:02:43 -0800

    fix js submit button

--- js/custom-subBtn.js
+++ js/custom-subBtn.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const email=document.getElementById("email");
+    const subForm = document.getElementById("subForm");
+    const email = document.getElementById("email");
+
+    subForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
         if (!email.checkValidity()) {
             alert("Please enter a valid e-mail");
         } else {
             alert("Thanks for subscribing!");
-            email.value = null;
+            email.value = "";
         }
+    });
 });
+
